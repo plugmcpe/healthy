d183f64c048a19f48a474816ea9c57de34c4dd25
plugmcpe <M7mmdgamer7@icloud.com>
2017-06-28 01:31:45 +0300

    Update main.php

--- src/healthy/main.php
+++ src/healthy/main.php
@@ -3,10 +3,12 @@ namespace healthy;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
-class extends main PluginBase{
+class Main extends PluginBase{
 public function onEnable(){
 $this->getServer()->getLogger()->info("plugin has been enabled");
 $this->getLogger()->info("everything is good");
  }
  public function onDisable(){
- $this->getServer()->getLogger()->info("plugin has been disable");
+ $this->getServer()->getLogger()->info("plugin has been disable"); 
+  }
+}
