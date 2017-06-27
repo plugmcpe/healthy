0355c3f5f9a2f1a38152040f1b2245dd4314e8cf
plugmcpe <M7mmdgamer7@icloud.com>
2017-06-28 02:30:04 +0300

    Update main.php

--- src/healthy/main.php
+++ src/healthy/main.php
@@ -11,10 +11,10 @@ $this->getLogger()->info("everything is good");
  public function onDisable(){
  $this->getServer()->getLogger()->info("plugin has been disable"); 
   }
-}
 public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
 switch ($cmd->getName()){
  case 'heal':
   $sender->setHealth(20);
+  }
  }
-}
+} 
